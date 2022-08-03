8afe5cdc34e454243f17fe993ed7f86c63145e6b
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-08-02 20:46:46 -0400

    Basic, but generalized way to input 3 numbers into the quadratic formula

---
+++ QuadraticFormula.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  // initialize variables
+  double coeff[3];
+  char letters[4] = "abc";
+  double root1;
+  double root2;
+  double discriminant;
+
+
+  // have user enter the "a" coefficient
+  for (int i = 0; i < 3; i++){
+    std::cout << "Please enter the \"" << letters[i] << "\" coefficient: " ;
+    std::cin >> coeff[i];
+
+  }
+  
+  
+  discriminant = (coeff[1]*coeff[1])-(4*coeff[0]*coeff[2]);
+  if (discriminant < 0){
+    double pdisc = abs(discriminant);
+    double one = (-coeff[1]/(2*coeff[0]));
+    double two = pdisc/(2*coeff[0]);
+
+    std::cout << "The answers are the complex roots: \n" << one << " + " << two << "i\n" << one << " - " << two << "i\n";
+  } else{
+    root1 = (-coeff[1] + sqrt((coeff[1]*coeff[1])-(4*coeff[0]*coeff[2])))/(2*coeff[0]);
+    root2 = (-coeff[1] - sqrt((coeff[1]*coeff[1])-(4*coeff[0]*coeff[2])))/(2*coeff[0]);
+    std::cout << "The additive root is: " << root1 << "\nThe subtractive root is:" << root2;
+  }
+  
+}
