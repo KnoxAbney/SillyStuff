1c75c3bbad048c512c99aefc303728f16d97a98f
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-28 11:33:24 -0400

    Update Whale_Translation.cpp
    Cleanup and comments

--- Whale_Translation.cpp
+++ Whale_Translation.cpp
@@ -4,16 +4,18 @@
 
 int main() {
 
+  // Initialize variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   std::string phrase;
   std::vector<char> whale;
 
+  // Prompt and accept input for a phrase to translate into "whale" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   std::cout << "Please enter a phrase to translate to whale:\n";
-
   getline(std::cin, phrase);
 
+  //Devtool
   //std::cout << phrase;
 
-
+  // Translate to whale. Only vowels are pronounced, and e's and u's are doubled. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   for (int i = 0; i < phrase.size(); i++){
       if (phrase[i] == 'e' || phrase[i] == 'u'){
         whale.push_back(phrase[i]);
@@ -23,8 +25,8 @@ int main() {
       }
   }
 
+  // Output phrase in whale ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   std::cout << "Your phrase translated to whale is:\n";
-
   for (int k = 0; k < whale.size(); k++){
 
     std::cout << whale[k];
