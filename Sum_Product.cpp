9e338e46848c8e47bcc9672c302a5fa219367f0b
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-27 20:15:19 -0400

    Update Sum_Product.cpp
    Tested and updated

--- Sum_Product.cpp
+++ Sum_Product.cpp
@@ -5,40 +5,35 @@ using namespace std;
 
 int main(){
 
-vector<int> numbers;
-vector<string> endings = {"st","nd","rd"};
-int number;
-int even;
-int odd;
+  vector<int> numbers;
+  vector<string> endings = {"st","nd","rd","th","th","th"};
+  int number;
+  int even = 0;
+  int odd = 1;
 
-cout << "You must enter 6 numbers one at a time and press 'enter' after each. The program will then output the sum of the even numbers and the product of the odd numbers.\n";
+  cout << "You must enter 6 numbers one at a time and press 'enter' after each. The program will then output the sum of the even numbers and the product of the odd numbers.\n";
 
-for(int i = 0; i < 6; i++){
+  for(int i = 0; i < 6; i++){
 
-  if (i<4){
-  cout << "\nPlease enter the " << i+1 << endings[i] << " number you would like to use:\n";
-  }else{
-  cout << "\nPlease enter the " << i+1 << "th number you would like to use:\n";
+    cout << "\nPlease enter the " << i+1 << endings[i] << " number you would like to use:\n";
+    cin >> number;
+    numbers.push_back(number);
+    
+  }
 
-  cin >> number;
-  numbers.push_back(number);
-  
-}
-}
+  for(int j = 0; j < numbers.size(); j++){
 
-for(int j = 0; j < numbers.size(); j++){
-
-  if (numbers[j]%2 == 0){
-
-    even = numbers[j] + even;
-
-  }else{
-
-    odd = numbers[j] * odd;
+    if (numbers[j]%2 == 0){
+      even = numbers[j] + even;
+    }else{
+      odd = numbers[j] * odd;
+    }
 
   }
+
+  cout << "The sum of the even numbers is: " << even << "\n";
+
+  cout << "The product of the odd numbers is: " << odd << "\n";
+
+
 }
-
-cout << "The sum of the even numbers is: " << even << "\n";
-
-cout << "The product of the odd numbers is: " << odd << "\n";
