cd1a0f60d95b5cb5733a36bdc5ddd784e5492151
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-28 11:29:52 -0400

    Update Water.cpp
    Cleanup and comments

--- Water.cpp
+++ Water.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-// Define needs_water() here:
+// Do it be needing water though? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 std::string needs_water(int days, bool is_succulent){
   if((is_succulent == false) && (days > 3)){
     return "Time to water the plant.";
   }else if((is_succulent == true) && (days >= 13)){
-    return "Go ahead and give the plant a little water.";
+    return "Go ahead and give the plant a little water. As a treat.";
   }else{
     return "Don't water the plant!";
   }
 }
 
-
+// Return input when prompted for days without water ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int days(){
   std::string days;
   std::cout << "How many days has it been since you last watered your plant?";
@@ -19,7 +19,7 @@ int days(){
   return days;
 }
 
-
+// Return input as boolean when asked whether the plant is a succulent ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 bool succ(){
   std::string temp
   bool succ;
